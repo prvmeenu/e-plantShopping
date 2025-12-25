7c1e87c40095740b27dae0e1eb1e3be8ecb1ed66
prvmeenu <prvmeenu@gmail.com>
2025-12-25 17:30:33 -0500

    Changes have been made.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+    const {name,img,cost} = action.payload;
+    //check if the itemis already exists in the cart
+    const exisistingItem = state.items.find(item => item.name === name);
+    if(exisistingItem){
+        //if exists true
+        exisistingItem.quantity++;
+    }else{
+        state.items.push({name,image,cost,quantity:1})
+    }
     },
     removeItem: (state, action) => {
+        const exisitingItem = state.items.filter(item => item.name !== action.payload);        
     },
     updateQuantity: (state, action) => {
-
+        const {name,quantity} = action.payload;
+        const updatingItem = state.items.find(item => item.name === name);
+        if(updatingItem){
+            updatingItem.quantity = quantity;
+        }
     
     },
   },
